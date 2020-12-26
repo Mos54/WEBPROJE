21769c2105365bbb3482e38f8024e9a4b63ba22c
DumanOlgun <olgun.duman1@ogr.sakarya.edu.tr>
2020-12-26 14:01:25 +0300

    Areas/Customer

---
+++ Customer/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using OnlineShop.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+        
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET Checkout action method
+
+        public IActionResult Chechout()
+        {
+            return View();
+        }
+
+        //POST Checkout action method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    OrderDetails orderDetails = new OrderDetails();
+                    orderDetails.ProductId = product.Id;
+                    anOrder.OrderDetails.Add(orderDetails);
+                }
+            }
+            anOrder.OrderNo = GetOrderNo();
+            _db.Orders.Add(anOrder);
+            await _db.SaveChangesAsync();
+            HttpContext.Session.Set("products", null);
+            return View();
+        }
+
+        public string GetOrderNo()
+        {
+            int rowCount = _db.Orders.ToList().Count()+1;
+            return rowCount.ToString("000");
+
+        }
+
+    }
+}
