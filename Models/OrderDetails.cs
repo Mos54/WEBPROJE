5d5197942867de133bd203d3ae549f6782474ef0
DumanOlgun <olgun.duman1@ogr.sakarya.edu.tr>
2020-12-27 14:08:31 +0300

    Models

---
+++ Models/OrderDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        [Display(Name="Order")]
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+        [ForeignKey("ProductId")]
+        public Products Porduct { get; set; }
+    }
+}
